4ce91d120d70d359515abd9566ac2a6018d24c5b
Rifkynur <ythrifky619@gmail.com>
2025-04-29 18:06:24 +0700

    feat: add protected in dasboard page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,29 @@
+"use client";
 import SidebarDashboard from "@/components/page/dashboard/SidebarDashboard";
 import NavbarDashboard from "@/components/page/dashboard/NavbarDashboard";
 import "../globals.css";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const userData = localStorage.getItem("user");
+    if (!token) {
+      router.push("/login");
+      return;
+    }
+    if (userData) {
+      const parsedData = JSON.parse(userData);
+      const userRole = parsedData?.state?.user?.role;
+      if (userRole !== "Admin") {
+        router.push("/");
+      }
+    }
+  }, []);
+
   return (
     <html>
       <Head>
